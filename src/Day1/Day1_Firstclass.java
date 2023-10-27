c52d5286327f8110f4f11f6972c0252eb5ec3443
unknown <vsujankumarreddy@gmail.com>
2023-10-27 06:44:22 +0530

    First commit for eclips

---
+++ src/Day1/Day1_Firstclass.java
@@ -0,0 +1,31 @@
+package Day1;
+
+public class Day1_Firstclass {
+	
+	static int a;
+	char b;
+	double c;
+	boolean d;
+
+	public static void main(String[] args) {
+		
+		byte f;
+		short g;
+		f=12;
+		g=22;
+		
+		System.out.println(f);
+		
+		
+		Day1_Firstclass dfc=new Day1_Firstclass();
+		
+		
+		System.out.println(dfc.a);
+		System.out.println(dfc.b);
+		System.out.println(dfc.c);
+		System.out.println(dfc.d);
+		System.out.println(f);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Java_Course_oct2023 {
+}
